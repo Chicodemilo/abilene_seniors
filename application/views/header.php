9eb5515d5a4e19cbd0086ef0252f678fb04c0c00
Miles Chick <mileschick@gmail.com>
2015-09-11 10:13:50 -0500

    Adding Google Tracking

--- application/views/header.php
+++ application/views/header.php
@@ -17,7 +17,14 @@
     <link rel="stylesheet" href="<?php echo base_url();?>malihu-custom-scrollbar-plugin-master/jquery.mCustomScrollbar.css" />
     <script src="<?php echo base_url();?>malihu-custom-scrollbar-plugin-master/jquery.mCustomScrollbar.concat.min.js"></script>
     <script>
-        // Google Analytics
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-67462114-1', 'auto');
+      ga('send', 'pageview');
+
     </script>
 
     <style type="text/css">

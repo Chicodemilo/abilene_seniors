f0d89ea27aa492007fd2c1d3bc38c0ad6a54d077
Miles Chick <mileschick@gmail.com>
2015-09-11 10:21:52 -0500

    Fixing Analytics Hopefully

--- application/views/blog_header.php
+++ application/views/blog_header.php
@@ -22,7 +22,7 @@
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-67462114-1', 'auto');
+      ga('create', 'UA-67486424-1', 'auto');
       ga('send', 'pageview');
 
     </script>

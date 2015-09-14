e277cb237c0c621969c92d308311ce9a690d7519
Miles Chick <mileschick@gmail.com>
2015-09-14 11:08:30 -0500

    Moble & Tab Search 1

--- application/views/blog_header.php
+++ application/views/blog_header.php
@@ -9,13 +9,14 @@
     <meta charset="utf-8" />
     <meta name="description" content="<?php echo $blog[0]['title']; ?>">
     <meta name="keywords" content="<?php echo $blog[0]['keyword_1']; ?>, <?php echo $blog[0]['keyword_2']; ?>, <?php echo $blog[0]['keyword_3']; ?>">
-    <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link href="<?php echo base_url();?>css/redmond/jquery-ui-1.10.4.custom.css" rel="stylesheet">
     <script src="<?php echo base_url();?>javascript/jquery-1.10.2.js"></script>
     <script src="<?php echo base_url();?>javascript/jquery-ui-1.10.4.custom.js"></script>
     <script src="https://code.createjs.com/createjs-2015.05.21.min.js"></script>
    <script type="text/javascript" src="<?php echo base_url();?>javascript/seniors_javascript.js"></script>
-    <link rel='stylesheet' media='only screen and (max-width: 1094px)' href='<?php echo base_url();?>css/abilene_seniors_main_small.css'/>
+    <link rel='stylesheet' media='only screen and (max-width: 1084px)' href='<?php echo base_url();?>css/abilene_seniors_main_med.css'/>
+    <link rel='stylesheet' media='only screen and (max-width: 700px)' href='<?php echo base_url();?>css/abilene_seniors_main_small.css'/>
     <link rel="stylesheet" href="<?php echo base_url();?>malihu-custom-scrollbar-plugin-master/jquery.mCustomScrollbar.css" />
     <script src="<?php echo base_url();?>malihu-custom-scrollbar-plugin-master/jquery.mCustomScrollbar.concat.min.js"></script>
     <script>
